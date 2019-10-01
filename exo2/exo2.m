979a7dd01628f46eff03a9e2b911868f7b27ff19
Belinda Schepers <belindaschepers1@gmail.com>
2019-10-01 09:35:22 +0200

    ajout repo

---
+++ exo2/exo2.m
@@ -0,0 +1,16 @@
+Whoami (who am i) est une commande UNIX permettant d'afficher le nom de l'usager associé à l'identificateur effectif (user ID ou UID) courant de 
+l'usager.
+
+Pwd est une commande UNIX. Elle permet d'afficher le chemin d'accès vers le répertoire où se situe l'utilisateur qui a entré la commande. 
+
+Chown est un appel système et une commande UNIX nécessitant les droits de l'utilisateur racine (root) pour changer le propriétaire d'un 
+fichier ou d'un dossier.
+
+Clear est une commande UNIX permettant de replacer la ligne de commande en haut de l'écran.
+
+
+Print-screens
+
+/home/belinda/Bureau/exo_terminal/assets/Capture d’écran de 2019-09-30 16-53-22.png
+/home/belinda/Bureau/exo_terminal/assets/Capture d’écran de 2019-09-30 17-05-47.png
+/home/belinda/Bureau/exo_terminal/assets/Capture d’écran de 2019-09-30 17-09-06.png 
